69edd0ae80f77a2986ada082b4d0c7e087b77597
sorcerer_com <sorcerer_com@abv.bg>
2021-03-09 13:38:27 +0200

    UI: Auto refresh, security and media player as pop-ups.

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,4 +1,17 @@
-﻿function addTextToForm(formId, inputName, text) {
+﻿function updateVue(data) {
+	if (!window.vue) {
+		window.vue = new Vue({
+			el: "#vue-content",
+			data: data
+		});
+	} else {
+		for (let key in data) {
+			Vue.set(window.vue, key, data[key]);
+        }
+    }
+}
+
+function addTextToForm(formId, inputName, text) {
 	var form = document.getElementById(formId);
 	var element = form.elements[inputName];
 	element.value += text;
