e3052156895af56d5a3ad36cfb3f0a4af9f17d31
sorcerer_com <sorcerer_com@abv.bg>
2022-05-05 17:46:55 +0300

    Allow edit settings as json.

--- UI/components/generic/monacoEditorComponent.js
+++ UI/components/generic/monacoEditorComponent.js
@@ -3,7 +3,7 @@ var templateUrl = scriptSrc.substr(0, scriptSrc.lastIndexOf(".")) + ".html";
 $.get(templateUrl, template => {
     Vue.component("monaco-editor", {
         template: template,
-        props: ["value"],
+        props: ["value", "type", "schema"],
         data: function () {
             return {
                 editor: null
@@ -12,32 +12,38 @@ $.get(templateUrl, template => {
         methods: {
         },
         mounted: function () {
-            getTypescriptModels().done(models => {
+            if (this.type == null || this.type == "typescript") {
                 monaco.languages.typescript.javascriptDefaults.setEagerModelSync(true);
-                monaco.editor.getModels().forEach(model => model.dispose()); // clear the models
-                monaco.editor.createModel(models, "typescript");
-
-                // create editor
-                var h_div = document.getElementById('monaco_editor');
-                this.editor = monaco.editor.create(h_div, {
-                    value: this.value,
-                    language: 'typescript',
-                    contextmenu: false,
-                    lineNumbers: true,
-                    lineNumbersMinChars: 3,
-                    lineDecorationsWidth: 0,
-                    minimap: { enabled: false }
-                });
                 monaco.languages.typescript.typescriptDefaults.setCompilerOptions(
                     {
                         noLib: true,
                         allowNonTsExtensions: true
                     });
-                this.editor.getModel().updateOptions({ tabSize: 2 });
-                this.editor.getModel().onDidChangeContent(_ => {
-                    // to work the v-model binding
-                    this.$emit("input", this.editor.getModel().getValue(monaco.editor.EndOfLinePreference.LF));
+
+                monaco.editor.getModels().forEach(model => model.dispose()); // clear the models
+                getTypescriptModels().done(models => monaco.editor.createModel(models, "typescript"));
+            }
+            else if (this.type == "json") {
+                monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+                    schemas: [{ fileMatch: ["*"], schema: this.schema }]
                 });
+            }
+
+            // create editor
+            var h_div = document.getElementById('monaco_editor');
+            this.editor = monaco.editor.create(h_div, {
+                value: this.value,
+                language: this.type ?? 'typescript',
+                contextmenu: false,
+                lineNumbers: true,
+                lineNumbersMinChars: 3,
+                lineDecorationsWidth: 0,
+                minimap: { enabled: false }
+            });
+            this.editor.getModel().updateOptions({ tabSize: 2 });
+            this.editor.getModel().onDidChangeContent(_ => {
+                // to work the v-model binding
+                this.$emit("input", this.editor.getModel().getValue(monaco.editor.EndOfLinePreference.LF));
             });
         }
     });
--- UI/components/generic/objectEditorComponent.js
+++ UI/components/generic/objectEditorComponent.js
@@ -6,7 +6,24 @@ $.get(templateUrl, template => {
         props: ["object", "settings", "onsave"],
         data: function () {
             return {
+                showEditor: false,
                 error: ""
+            }
+        },
+        computed: {
+            json: {
+                get: function () {
+                    console.log(this.getJsonSchema(this.object["$subtypes"]));
+
+                    let filteredObject = Object.keys(this.object)
+                        .filter(key => key[0] != "$" && (this.settings == null || this.object['$subtypes'][key].setting == this.settings))
+                        .reduce((cur, key) => { return Object.assign(cur, { [key]: this.object[key] }) }, {});
+                    return JSON.stringify(filteredObject, null, 2);
+                },
+                set: function (newValue) {
+                    let obj = JSON.parse(newValue);
+                    Object.keys(obj).forEach(key => this.object[key] = obj[key]);
+                }
             }
         },
         methods: {
@@ -17,6 +34,57 @@ $.get(templateUrl, template => {
                 this.onsave(this.object).fail(response => {
                     this.error = "Error: " + response.responseText;
                 });
+            },
+
+            getJsonSchema: function (subtype) {
+                let schema = {};
+                if (!subtype.type) {
+                    schema.type = "object";
+                    schema.properties = {};
+                    for (let key of Object.keys(subtype)) {
+                        schema.properties[key] = this.getJsonSchema(subtype[key]);
+                    }
+                } else {
+                    schema.type = subtype.type.toLowerCase();
+                    schema.description = subtype.hint;
+
+                    if (subtype.type.startsWith("Int") || subtype.type == "Double")
+                        schema.type = "number";
+                    else if (subtype.type == "DateTime") {
+                        schema.type = "string";
+                        schema.pattern = "^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$";
+                    }
+                    else if (subtype.type == "TimeSpan") {
+                        schema.type = "string";
+                        schema.pattern = "^([0-9]*\\.)?[0-9]{2}:[0-9]{2}:[0-9]{2}$";
+                    }
+                    else if (subtype.type == "ValueTuple") {
+                        schema.type = "string";
+                        schema.pattern = "^\\(.*, .*\\)$";
+                    }
+                    else if (subtype.enums) {
+                        delete schema.type;
+                        schema.enum = subtype.enums.map(v => subtype.type + "." + v);
+                    }
+                    else if (subtype.type == "select") {
+                        delete schema.type;
+                        schema.enum = Object.keys(subtype.select).concat([""]);
+                    }
+                    else if (subtype.type == "List") {
+                        schema.type = "array";
+                        schema.items = this.getJsonSchema(subtype["genericTypes"][0]);
+                    }
+                    else if (subtype.type == "Dictionary") {
+                        schema.type = "object";
+                        schema.additionalProperties = this.getJsonSchema(subtype["genericTypes"][1]);
+                    }
+                }
+                return schema;
+            }
+        },
+        watch: {
+            "object": function () {
+                this.showEditor = false;
             }
         }
     });
