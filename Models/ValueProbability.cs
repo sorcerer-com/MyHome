e350e204e72a532cd4d98e1b72a112a38aca01cc
sorcerer_com <sorcerer_com@abv.bg>
2022-12-09 14:41:55 +0200

    Prediction system

---
+++ Models/ValueProbability.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using NLog;
+
+namespace MyHome.Models
+{
+    public class ValueProbability
+    {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        private readonly int minSamples;
+        private readonly double cleanupThreshold;
+        [JsonProperty]
+        private readonly Dictionary<string, (int Samples, int Count)> data; // value / (samples / count)
+
+        public ValueProbability(int minSamples = 10, double cleanupThreshold = 0.1)
+        {
+            this.minSamples = minSamples;
+            this.cleanupThreshold = cleanupThreshold;
+            this.data = new Dictionary<string, (int samples, int count)>();
+        }
+
+        public void AddValue(string value, int sample = 1, bool maxCountInit = false)
+        {
+            this.Cleanup(maxCountInit);
+            if (!this.data.ContainsKey(value))
+            {
+                if (sample == 0)
+                    return;
+
+                int count = maxCountInit && this.data.Count > 0 ? this.data.Values.Max(d => d.Count) : 0;
+                this.data[value] = (0, count);
+            }
+
+            this.data[value] = (this.data[value].Samples + sample, this.data[value].Count + 1);
+            logger.Debug($"Add value: {value}, sample: {sample}, count: {this.data[value].Count}");
+        }
+
+        public double GetProbability(string value)
+        {
+            if (!this.data.ContainsKey(value))
+            {
+                logger.Debug($"No statistic for this value: {value}");
+                return 0;
+            }
+            // if there is not enough statistic data
+            if (this.data[value].Count < this.minSamples)
+            {
+                logger.Debug($"Cannot get probability, count: {this.data[value].Count}, minSamples: {this.minSamples}");
+                return -1;
+            }
+
+            var prob = (double)this.data[value].Samples / this.data[value].Count;
+            logger.Debug($"Value: {value}, probability: {prob}");
+            return prob;
+        }
+
+        private void Cleanup(bool thresholdOfMaxCount)
+        {
+            var toRemove = new List<string>();
+            int maxCount = thresholdOfMaxCount && this.data.Count > 0 ? this.data.Values.Max(d => d.Count) : 1;
+            foreach (var kvp in this.data)
+            {
+                var prob = (double)kvp.Value.Samples / kvp.Value.Count;
+                if (prob < this.cleanupThreshold / maxCount)
+                {
+                    logger.Debug($"Removing value: {kvp.Key}, prob: {prob}, threshold: {this.cleanupThreshold}");
+                    toRemove.Add(kvp.Key);
+                }
+            }
+
+            foreach (var value in toRemove)
+                this.data.Remove(value);
+        }
+    }
+}
---
+++ Systems/PredictionSystem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MyHome.Models;
+using MyHome.Utils;
+
+using Newtonsoft.Json;
+
+namespace MyHome.Systems
+{
+    public class PredictionSystem : BaseSystem
+    {
+        public static readonly string DataFilePath = Path.Join(Config.BinPath, "probabilities.json");
+
+        public enum ProbabilityType
+        {
+            NoPresence
+        }
+
+        private readonly Dictionary<ProbabilityType, ValueProbability> probabilities;
+        private int hourUpdate;
+
+        public PredictionSystem()
+        {
+            this.probabilities = new Dictionary<ProbabilityType, ValueProbability>
+            {
+                [ProbabilityType.NoPresence] = new ValueProbability()
+            };
+
+            this.hourUpdate = DateTime.Now.Hour;
+        }
+
+        public override void Setup()
+        {
+            base.Setup();
+            if (File.Exists(DataFilePath))
+            {
+                var json = File.ReadAllText(DataFilePath);
+                JsonConvert.PopulateObject(json, this.probabilities);
+            }
+
+            MyHome.Instance.Events.Handler += this.Events_Handler;
+        }
+
+        private void Events_Handler(object sender, GlobalEventArgs e)
+        {
+            if (e.EventType == GlobalEventTypes.PresenceChanged)
+            {
+                var now = DateTime.Now;
+                this.probabilities[ProbabilityType.NoPresence].AddValue($"{(int)now.DayOfWeek}{now.Hour}", (bool)e.Data ? 0 : 1, true);
+                // save
+                var json = JsonConvert.SerializeObject(this.probabilities, Formatting.Indented);
+                File.WriteAllText(DataFilePath, json);
+            }
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            var now = DateTime.Now;
+            if (now.Hour != this.hourUpdate)
+            {
+                this.hourUpdate = now.Hour;
+
+                this.probabilities[ProbabilityType.NoPresence].AddValue($"{(int)now.DayOfWeek}{now.Hour}", MyHome.Instance.SecuritySystem.Presence ? 0 : 1, true);
+                // save
+                var json = JsonConvert.SerializeObject(this.probabilities, Formatting.Indented);
+                File.WriteAllText(DataFilePath, json);
+            }
+        }
+    }
+}
